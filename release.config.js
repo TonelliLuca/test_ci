f0b99cb0e10c9f36655c94dc52aba6d1710c21ef
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-16 23:06:07 +0100

    feat: fef

--- release.config.js
+++ release.config.js
@@ -1,13 +1,7 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 
 config.plugins.push(
-  [
-    "@semantic-release/git",
-    {
-      assets: ["package.json", "CHANGELOG.md"],
-      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }
-  ],
+  "@semantic-release/git",
   "@semantic-release/github",
   [
     "@semantic-release/exec",
@@ -16,16 +10,6 @@ config.plugins.push(
       publishCmd: 'echo "Publishing version ${nextRelease.version}"',
       successCmd: 'echo "::set-output name=release_created::true"',
       failCmd: 'echo "::set-output name=release_created::false"'
-    }
-  ],
-  [
-    "semantic-release-github-pullrequest",
-    {
-      assets: ["package.json", "CHANGELOG.md"],
-      baseRef: "main",
-      title: "chore(release): ${nextRelease.version}",
-      body: "This pull request includes the changes for version ${nextRelease.version}.",
-      branch: "release-${nextRelease.version}"
     }
   ]
 );
