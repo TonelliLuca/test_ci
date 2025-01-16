9158e7a08e7249299cadd3ea9a8a8edea11662ca
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-17 00:49:19 +0100

    feat: sdsdsds

--- release.config.mjs
+++ release.config.mjs
@@ -1,25 +1,24 @@
-import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
-
-export default {
-  ...config,
-  plugins: [
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" }
+config.plugins.push(
     [
-      "semantic-release-github-pullrequest",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        baseRef: "main",
-        title: "chore(release): ${nextRelease.version}",
-        body: "This pull request includes the changes for version ${nextRelease.version}.",
-        branch: "release-${nextRelease.version}"
-      }
+        "semantic-release-github-pullrequest",
+        {
+            assets: ["package.json", "CHANGELOG.md"],
+            baseRef: "main",
+            title: "chore(release): ${nextRelease.version}",
+            body: "This pull request includes the changes for version ${nextRelease.version}.",
+            branch: "release-${nextRelease.version}"
+        }
     ],
     "@semantic-release/github",
     [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]"
-      }
+        "@semantic-release/git",
+        {
+            assets: ["package.json", "CHANGELOG.md"],
+            message: "chore(release): ${nextRelease.version} [skip ci]"
+        }
     ]
-  ]
-};
+)
+
+
+export default config
