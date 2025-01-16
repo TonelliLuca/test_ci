afc9d4ffe964364dbbd4254eeb3e993be67c5da9
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-16 22:01:29 +0100

    feat: wedffwù

--- release.config.js
+++ release.config.js
@@ -1,6 +1,7 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 
 config.plugins.push(
+    "@semantic-release/git",
   "@semantic-release/github",
   [
     "@semantic-release/npm",
