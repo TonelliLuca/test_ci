20b42524048418a9cfbd795ab00c61aa9427ac48
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-16 21:05:22 +0100

    feat: new config

--- release.config.js
+++ release.config.js
@@ -1,24 +1,26 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 
 config.plugins.push(
-    "@semantic-release/github",
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: 'echo "Preparing version ${nextRelease.version}"',
-        publishCmd: 'echo "Publishing version ${nextRelease.version}"',
-        successCmd: 'echo "::set-output name=release_created::true"',
-        failCmd: 'echo "::set-output name=release_created::false"'
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]"
-      }
-    ]
+  "@semantic-release/github",
+  [
+    "@semantic-release/exec",
+    {
+      prepareCmd: 'echo "Preparing version ${nextRelease.version}"',
+      publishCmd: 'echo "Publishing version ${nextRelease.version}"',
+      successCmd: 'echo "::set-output name=release_created::true"',
+      failCmd: 'echo "::set-output name=release_created::false"'
+    }
+  ],
+  [
+    "semantic-release-github-pullrequest",
+    {
+      assets: ["package.json", "CHANGELOG.md"],
+      baseRef: "main",
+      title: "chore(release): ${nextRelease.version}",
+      body: "This pull request includes the changes for version ${nextRelease.version}.",
+      branch: "release-${nextRelease.version}"
+    }
+  ]
 );
-  
 
 module.exports = config;
