af21965850c282e03cf989b5272fdc68a2a79e96
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-16 20:50:51 +0100

    feat: adjust config

--- release.config.js
+++ release.config.js
@@ -1,17 +1,24 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 
 config.plugins.push(
-    "@semantic-release/git",
-  "@semantic-release/github",
-  [
-    "@semantic-release/exec",
-    {
-      prepareCmd: 'echo "Preparing version ${nextRelease.version}"',
-      publishCmd: 'echo "Publishing version ${nextRelease.version}"',
-      successCmd: 'echo "::set-output name=release_created::true"',
-      failCmd: 'echo "::set-output name=release_created::false"'
-    }
-  ]
+    "@semantic-release/github",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: 'echo "Preparing version ${nextRelease.version}"',
+        publishCmd: 'echo "Publishing version ${nextRelease.version}"',
+        successCmd: 'echo "::set-output name=release_created::true"',
+        failCmd: 'echo "::set-output name=release_created::false"'
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]"
+      }
+    ]
 );
+  
 
 module.exports = config;
