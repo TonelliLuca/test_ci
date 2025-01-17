4086ac60dbbf7158c93c9255c4db4fb5e5c95d7d
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-17 02:59:50 +0100

    feat: add a

--- release.config.mjs
+++ release.config.mjs
@@ -1,8 +1,19 @@
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" }
 
 config.plugins.push(
+
+    [
+        "semantic-release-github-pullrequest",
+        {
+            assets: ["package.json", "CHANGELOG.md"],
+            baseRef: "main",
+            title: "chore(release): ${nextRelease.version}",
+            body: "This pull request includes the changes for version ${nextRelease.version}.",
+            branch: "release/${nextRelease.version}",
+        }
+    ],
     "@semantic-release/github"
 )
-
+config.branches = ["main"]
 
 export default config
