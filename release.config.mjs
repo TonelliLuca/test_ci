9d1291b0633ecae5b758be4e2628db1682e5f4a9
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-17 01:24:30 +0100

    feat: add ci

--- release.config.mjs
+++ release.config.mjs
@@ -1,39 +1,24 @@
-import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" }
 
-// Define the release branch name
-const releaseBranch = `release-${nextRelease.version}`;
-
-// Add a step to create a new branch before the prepare step
-config.plugins.unshift({
-  verifyConditions: [],
-  prepare: [
-    {
-      path: '@semantic-release/exec',
-      cmd: `git checkout -b ${releaseBranch}`
-    }
-  ]
-});
-
-// Configure the plugins
 config.plugins.push(
-  [
-    "@semantic-release/git",
-    {
-      assets: ["package.json", "CHANGELOG.md"],
-      message: "chore(release): ${nextRelease.version} [skip ci]",
-      branch: releaseBranch
-    }
-  ],
-  [
-    "semantic-release-github-pullrequest",
-    {
-      assets: ["package.json", "CHANGELOG.md"],
-      baseRef: "main",
-      title: "chore(release): ${nextRelease.version}",
-      body: "This pull request includes the changes for version ${nextRelease.version}.",
-      branch: releaseBranch
-    }
-  ]
-);
+    [
+        "semantic-release-github-pullrequest",
+        {
+            assets: ["package.json", "CHANGELOG.md"],
+            baseRef: "main",
+            title: "chore(release): ${nextRelease.version}",
+            body: "This pull request includes the changes for version ${nextRelease.version}.",
+            branch: "release-${nextRelease.version}"
+        }
+    ],
+    [
+        "@semantic-release/git",
+        {
+            assets: ["package.json", "CHANGELOG.md"],
+            message: "chore(release): ${nextRelease.version} [skip ci]"
+        }
+    ],
+    "@semantic-release/github"
+)
 
-export default config;
+export default config
